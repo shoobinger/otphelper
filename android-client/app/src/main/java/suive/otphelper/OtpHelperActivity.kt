ab8f2d6633f957991b54ed96035f118a7a828540
suive <is@suive.co>
2021-02-23 21:58:50 +0300

    Handle permissions

--- android-client/app/src/main/java/suive/otphelper/OtpHelperActivity.kt
+++ android-client/app/src/main/java/suive/otphelper/OtpHelperActivity.kt
@@ -8,6 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 
 class OtpHelperActivity : AppCompatActivity() {
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,7 +20,7 @@ class OtpHelperActivity : AppCompatActivity() {
         ActivityCompat.requestPermissions(
             this,
             arrayOf(Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS),
-            10
+            PERMISSION_REQUEST_CODE
         )
     }
 
@@ -24,25 +28,14 @@ class OtpHelperActivity : AppCompatActivity() {
         requestCode: Int,
         permissions: Array<String>, grantResults: IntArray
     ) {
-        val granted = if (checkPermissionGranted(
-                requestCode,
-                permissions,
-                grantResults
-            )
-        ) "permission granted" else "permission not granted"
-        Toast.makeText(this, granted, Toast.LENGTH_SHORT).show()
-    }
-
-    fun checkPermissionGranted(
-        requestCode: Int,
-        permissions: Array<String>, grantResults: IntArray
-    ): Boolean {
-        when (requestCode) {
-            10 -> {
-                // If request is cancelled, the result arrays are empty.
-                return (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            }
+        val permissionsGranted = requestCode == PERMISSION_REQUEST_CODE &&
+            grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+        if (!permissionsGranted) {
+            Toast.makeText(
+                this,
+                "No permissions to read SMS: OTP Helper won't work correctly",
+                Toast.LENGTH_LONG
+            ).show()
         }
-        return false
     }
 }
--- android-client/app/src/main/java/suive/otphelper/SettingsFragment.kt
+++ android-client/app/src/main/java/suive/otphelper/SettingsFragment.kt
@@ -24,7 +24,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
                     ).show()
                     false
                 }
-
             }
     }
 }
